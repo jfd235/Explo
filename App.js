02b92a95c9ac580df02ab07e0601137259a53003
James Donnelly <donnellyfj@gmail.com>
2023-04-21 13:53:03 -0400

    Add files

---
+++ App.js
@@ -0,0 +1,22 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './screens/Home'
+import { ProfileScreen} from './screens/Profile'
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ screens/Profile.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export function ProfileScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile Screen</Text>
+      </View>
+    );
+  }
