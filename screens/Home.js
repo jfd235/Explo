bd508a5244c72273e5f59470bfa95a45220283e7
James Donnelly <donnellyfj@gmail.com>
2023-04-25 18:25:47 -0400

    Added some buttons

--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,43 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Map"
-        onPress={() => navigation.navigate('Map')}
-      />
+    <View style={[styles.container,{flexDirection: 'column',},]}>
+      {/* <Text>Home Screen</Text> */}
+      <View style={[buttonStyles.container]}>
+        <Button color="#FFFFFF"
+          title="Go to Map"
+          onPress={() => navigation.navigate('Map')}
+        />
+      </View>
+      <View style={[buttonStyles.container]}>
+        <Button color="#FFFFFF"
+          title="Go to Profile"
+          onPress={() => navigation.navigate('Profile')}
+        />
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    gap: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+const buttonStyles = StyleSheet.create({
+  container: {
+    width: 150,
+    height: 50,
+    backgroundColor: '#B6E13D',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+  },
+});
