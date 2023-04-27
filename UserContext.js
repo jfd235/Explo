d8f550d900d9b3d3c394a65d08fa8f5dd6254e0f
Kalva014 <kalva014@ucr.edu>
2023-04-27 17:37:26 -0400

    Fixed the weird state when not signed in and click on go to profile

--- UserContext.js
+++ UserContext.js
@@ -2,9 +2,12 @@ let currentUser;
 
 const setUserVariable = (variable) => {
     currentUser = variable;
+    console.log("user has been set")
+    console.log({currentUser})
 };
 
 const getUserVariable = () => {
+    console.log(currentUser)
   return currentUser;
 };
 
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,4 +1,3 @@
-// import * as React from 'react';
 import React, { useState, useEffect, useContext } from "react";
 import { View, Text, Image, Button, TextInput, StyleSheet } from 'react-native';
 import firebase from 'firebase/app';
@@ -7,38 +6,9 @@ import 'firebase/storage';
 import { auth } from "../firebaseConfig";
 import { getUserVariable } from '../UserContext';
 
-// async function uploadProfileImage(imageUri) {
-//   // const userId = firebase.auth().currentUser.uid;
-//   const userId = auth().currentUser.uid;
-//   const storageRef = firebase.storage().ref(`users/${userId}/profile.jpg`);
-
-//   const response = await fetch(imageUri);
-//   const blob = await response.blob();
-
-//   await storageRef.put(blob);
-
-//   const downloadUrl = await storageRef.getDownloadURL();
-//   return downloadUrl;
-// }
-
-// export function ProfileScreen() {
-//   let user = getUserVariable();
-
-//   if (!user) {
-//     console.log(user)
-//     // Handle the case where user object is not set yet
-//     return (
-//       <View>
-//         <Text>Loading...</Text>
-//       </View>
-//     );
-//   }
-// }
-
 let user = getUserVariable();
 
 async function uploadProfileImage(imageUri) {
-  // const userId = auth().currentUser.uid;
   const storageRef = firebase.storage().ref(`users/${user.uid}/profile.jpg`);
 
   const response = await fetch(imageUri);
@@ -53,65 +23,79 @@ async function uploadProfileImage(imageUri) {
 export function ProfileScreen() {
   const [name, setName] = useState("");
   const [profileImage, setProfileImage] = useState(null);
+  console.log("called again")
+  let user = getUserVariable();
 
-  useEffect(() => {
-    const user = getUserVariable();
-    setName(user.displayName);
-    setProfileImage(user.photoURL);
-  }, []);
-
-  const handleUpdateProfile = async () => {
-    try {
-      await user.updateProfile({
-        displayName: name,
-        photoURL: profileImage,
-      });
-      // Update user context or navigate to another screen
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleSelectProfileImage = async () => {
-    try {
-      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (status !== "granted") {
-        alert("Sorry, we need camera roll permissions to make this work!");
-        return;
-      }
-
-      const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true,
-        aspect: [1, 1],
-        quality: 1,
-      });
-
-      if (!result.cancelled) {
-        setProfileImage(result.uri);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.profileImageContainer}>
-        <Image source={{ uri: profileImage }} style={styles.profileImage} />
-        <Button title="Change Profile Image" onPress={handleSelectProfileImage} />
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <Text>Can't view since you haven't signed in or created an account</Text>
       </View>
-      <View style={styles.profileInfoContainer}>
-        <TextInput
-          style={styles.nameInput}
-          placeholder="Enter your name"
-          value={name}
-          onChangeText={setName}
-        />
-        <Button title="Update Profile" onPress={handleUpdateProfile} />
+    );
+  }
+
+  else {
+    useEffect(() => {
+      const user = getUserVariable();
+      setName(user.displayName);
+      setProfileImage(user.photoURL);
+      console.log(profileImage)
+    }, []);
+
+
+    const handleUpdateProfile = async () => {
+      try {
+        await user.updateProfile({
+          displayName: name,
+          photoURL: profileImage,
+        });
+        // Update user context or navigate to another screen
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+
+    const handleSelectProfileImage = async () => {
+      try {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+          return;
+        }
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
+        });
+
+        if (!result.cancelled) {
+          setProfileImage(result.uri);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.profileImageContainer}>
+          <Image source={{ uri: profileImage }} style={styles.profileImage} />
+          <Button title="Change Profile Image" onPress={handleSelectProfileImage} />
+        </View>
+        <View style={styles.profileInfoContainer}>
+          <TextInput
+            style={styles.nameInput}
+            placeholder="Enter your name"
+            value={name}
+            onChangeText={setName}
+          />
+          <Button title="Update Profile" onPress={handleUpdateProfile} />
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
